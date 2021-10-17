72a75aeedd9f3cdb6ee77441271e9f9e4035daf3
Jason Jobe <box2019@jasonjobe.com>
2021-10-16 20:54:34 -0400

    Package.swift cleanup

--- Package.swift
+++ Package.swift
@@ -12,7 +12,6 @@ let package = Package(
         .watchOS(.v6),
     ],
     products: [
-        // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
             name: "CQI",
             targets: ["CQI"]),
@@ -28,9 +27,7 @@ let package = Package(
 
     ],
     targets: [
-        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-        // Targets can depend on other targets in this package, and on products in packages this package depends on.
-        .target(
+         .target(
             name: "CQI",
             dependencies: [
                 "FeistyDB",
@@ -38,9 +35,7 @@ let package = Package(
                 .product(name: "Runtime", package: "Runtime"),
                 .product(name: "FeistyExtensions", package: "FeistyDB"),
                 .product(name: "Tagged", package: "swift-tagged"),
-
-                            ]
-//            ,exclude: ["Tests/__Snapshots__"]
+            ]
         ),
         .testTarget(
             name: "CQITests",
